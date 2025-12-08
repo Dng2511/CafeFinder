a25a8d47cd1c10e8f26adc510aedb54ac2808eac
Dng Pham <phamdong9854@gmail.com>
2025-12-08 20:13:42 +0700

    update findByDistance func

--- backend/src/routers/web.js
+++ backend/src/routers/web.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const CafeController = require('../apps/controllers/apis/cafe');
-const FavoriteController = require('../apps/controllers/apis/favorite');
+//const FavoriteController = require('../apps/controllers/apis/favorite');
 const router = express.Router();
 
 
@@ -10,8 +10,8 @@ router.get('/cafes', CafeController.index);
 router.get('/cafes/:id', CafeController.searchById);
 
 // Favorites routes
-router.post('/favorites', FavoriteController.addFavorite);
-router.delete('/favorites/:cafe_id', FavoriteController.removeFavorite);
-router.get('/favorites/:user_id', FavoriteController.getFavorites);
+//router.post('/favorites', FavoriteController.addFavorite);
+//router.delete('/favorites/:cafe_id', FavoriteController.removeFavorite);
+//router.get('/favorites/:user_id', FavoriteController.getFavorites);
 
 module.exports = router;
