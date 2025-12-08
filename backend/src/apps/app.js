6758b06a34cd79ae41e325016d488e4e4a239bb2
Dng Pham <phamdong9854@gmail.com>
2025-12-08 20:27:49 +0700

    u

--- backend/src/apps/app.js
+++ backend/src/apps/app.js
@@ -9,6 +9,5 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use("/", require(`${__dirname}/../routers/web`));
-app.set("json spaces", 2); 
 
 module.exports = app;
